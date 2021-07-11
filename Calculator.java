3b5cc37b2255233bdce890fd3eaefe38261c9d27
DimaSmyk <dima.smyk99.19@gmail.com>
2021-07-11 12:48:04 +0300

    2

--- Calculator.java
+++ Calculator.java
@@ -2,7 +2,12 @@ package selfwork06.task01;
 
 public class Calculator {
 
-    public int plus (int a, int b){
-        return a + b;
+    public int plus2() {
+        int[] array = new int[]{42, 12, 76, 21, 4, 10};
+        int sum = 0;
+        for (double element : array) {
+            sum += element;
+        }
+        return sum;
     }
 }
